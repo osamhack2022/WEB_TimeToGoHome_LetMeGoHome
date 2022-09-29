b72e0542fdb33beae569a0c3bc304c1724724f43
22thking <70695171+22thking@users.noreply.github.com>
2022-09-29 14:29:55 +0000

    chore(back): post login 테스트

--- WEB_BE/src/controller/api.js
+++ WEB_BE/src/controller/api.js
@@ -1,1 +1,21 @@
-//api 설정
+import express from "express";
+const app = express();
+let port = process.env.port || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+app.get("/api/hello", (req, res) => {
+  res.send("연결 완료");
+  console.log("11");
+});
+
+app.post("/api/login", (req, res)=> {
+    console.log(req);
+    res.send("로그인 완료");
+})
+
+const server = app.listen(port, () => {
+  console.log(`server on ${port}`);
+});
