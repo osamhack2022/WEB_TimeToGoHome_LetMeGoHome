a43872e0d85fb8e7a41215aecb958182e1cc398d
devgeon <devgeon.git@gmail.com>
2022-10-14 10:34:32 +0000

    feature(back): add morgan

--- WEB_BE/src/app.js
+++ WEB_BE/src/app.js
@@ -2,15 +2,18 @@ import express from "express";
 const app = express();
 let port = process.env.port || 5000;
 import bodyParser from "body-parser";
+import morgan from "morgan";
+
 import swaggerJsdoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
 import swaggerOptions from "./swagger.js";
 const specs = swaggerJsdoc(swaggerOptions);
+
 import apiRouter from "./routes/api.js";
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(bodyParser.json());
+app.use(morgan(process.env.NODE_ENV === "production" ? "combined" : "dev"));
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
