29846c9b76661ec2b6f38460e2340989729e33be
22thking <70695171+22thking@users.noreply.github.com>
2022-10-21 13:47:23 +0000

    featrue(back): User API 추가

--- WEB_BE/src/routes/api.js
+++ WEB_BE/src/routes/api.js
@@ -1,7 +1,10 @@
 import express from "express";
 const router = express.Router();
 import auth from "./auth/auth.js";
+import user from "./user/user.js";
 
 router.use("/auth", auth);
+
+router.use("/user", user);
 
 export default router;
--- WEB_BE/src/routes/auth/auth.js
+++ WEB_BE/src/routes/auth/auth.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { Prisma, PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const prisma = new PrismaClient();
 const router = express.Router();
@@ -21,14 +22,26 @@ router.post("/login", async (req, res) => {
     // 비밀번호가 틀리면 error
     res.status(400).json({ message: "invalid password" });
   }
-  // 비밀번호가 맞으면 로그인
-  res.status(200).json({ message: "login success" });
+  const token = jwt.sign(
+    {
+      id: data.id,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "7d",
+    }
+  );
+  // 비밀번호가 맞으면 로그인 및 토큰 발급
+  res.status(200).json({
+    token,
+    message: "login success",
+  });
 });
 
 router.post("/register", async (req, res) => {
   //password 암호화
   const salt = 10;
-  const enpassword = await bcrypt.hash(req.body.password,salt);
+  const enpassword = await bcrypt.hash(req.body.password, salt);
 
   try {
     const data = await prisma.user.create({
@@ -40,14 +53,14 @@ router.post("/register", async (req, res) => {
         armyRank: req.body.armyRank,
         enlistment: new Date(req.body.enlistment),
         discharge: new Date(req.body.discharge),
-        image: "https://i.imgur.com/3ZQ3Z0x.png",
+        image: "https://i.imgur.com/3ZQ3Z0x.png", // sample image
       },
     });
     res.status(200).json({ data });
   } catch (error) {
     if (error instanceof Prisma.PrismaClientKnownRequestError) {
       // The .code property can be accessed in a type-safe manner
-      if (error.code === 'P2002') {
+      if (error.code === "P2002") {
         const email = await prisma.user.findFirst({
           // email 찾기
           where: { email: req.body.email },
@@ -59,10 +72,51 @@ router.post("/register", async (req, res) => {
         } else {
           res.status(400).json({ message: "중복되는 email 입니다." });
         }
-      } else if(error.code === 'P2000') {
+      } else if (error.code === "P2000") {
         res.status(400).json({ message: "너무 긴 입력 값이 있습니다." });
       }
     }
+  }
+});
+
+//token 검증
+router.get("/check", async (req, res) => {
+  const token = req.headers["x-access-token"] || req.query.token;
+  if (!token) {
+    return res.status(403).json({
+      success: false,
+      message: "not logged in",
+    });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (decoded) {
+      const data = await prisma.user.findFirst({
+        where: { id: decoded.id },
+      });
+      if (!data || data.status === "DELETED") {
+        return res.status(403).json({
+          success: false,
+          message: "not logged in",
+        });
+      }
+      return res.status(200).json({
+        success: true,
+        info: data,
+      });
+    }
+  } catch (error) {
+    if (error.name === "TokenExpiredError") {
+      // 유효기간 초과
+      return res.status(419).json({
+        success: false,
+        message: "token expired",
+      });
+    }
+    return res.status(401).json({
+      success: false,
+      message: "invalid token",
+    });
   }
 });
 
