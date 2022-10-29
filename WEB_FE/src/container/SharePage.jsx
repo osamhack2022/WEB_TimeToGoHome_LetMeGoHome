66cee5e11a7ca95e329426d8307c41f3c5090a42
devgeon <devgeon.git@gmail.com>
2022-10-29 16:46:59 +0000

    refactor(front): refactor share page

--- WEB_FE/src/container/SharePage.jsx
+++ WEB_FE/src/container/SharePage.jsx
@@ -1,18 +1,11 @@
-/* eslint-disable react/prop-types */
 import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
 import axios from "axios";
-
-import bg from "../images/bg.jpg";
 
 function SharePage({ user, Logout }) {
   axios.defaults.headers.common["x-access-token"] =
     localStorage.getItem("token"); // setting token to axios header
-  const [hashtagList, setHashtagList] = useState([
-    "운동",
-    "자격증",
-    "공부",
-    "음악",
-  ]);
+  const [hashtagList, setHashtagList] = useState([]);
   const [todoLists, setTodoLists] = useState([]);
   const [hashtag, setHashtag] = useState("");
   useEffect(() => {
@@ -37,8 +30,8 @@ function SharePage({ user, Logout }) {
   }, [hashtag]);
 
   return (
-    <div className="w-[100vw] h-[100vh] font-StrongAF">
-      <nav className="bg-primary h-20 flex items-center justify-between">
+    <div className="w-full h-full font-StrongAF">
+      <nav className="bg-primary w-full h-20 flex items-center justify-between">
         <h1 className="font-StrongAFBold xl:text-4xl md:text-2xl text-xl ml-[45px] text-white">
           이젠 돌아갈 때
         </h1>
@@ -51,12 +44,12 @@ function SharePage({ user, Logout }) {
         </button>
       </nav>
       <div className="flex flex-row">
-        <div className="flex flex-col items-center justify-start text-2xl h-[91.5vh] w-[17%] bg-white">
+        <div className="flex flex-col items-center justify-start text-2xl h-full w-[17%] bg-white">
           {hashtagList.map((tag) => (
-            <div className="w-full flex justify-center">
+            <div className="w-full">
               <button
                 type="button"
-                className="w-full h-20 flex justify-center items-center"
+                className="w-full h-20"
                 onClick={() => {
                   setHashtag(tag);
                 }}
@@ -66,24 +59,24 @@ function SharePage({ user, Logout }) {
             </div>
           ))}
         </div>
-        <div className="dashboard flex flex-row bg-gray-200 w-screen h-screen">
+        <div name="dashboard" className="bg-gray-200 w-full h-fit min-h-screen">
           {/* title, description, hashtag, image */}
-          <div className="flex flex-col items-center text-2xl mx-auto">
+          <div className="mx-5 overflow-y-auto">
             {todoLists.map((todo) => (
-              <div>
+              <div className="mx-auto my-5 max-w-[1200px]">
                 <div>
                   <img
-                    className="pb-[30px] px-[300px]"
+                    className="object-cover"
                     src={todo.image}
                     alt="shareImage"
                   />
                 </div>
-                <div className="bg-white flex flex-row justify-between  w-[840px] h-[100px]">
+                <div className="bg-white w-full h-fit p-3">
                   <div>
-                    <span className="p-5">{todo.title}</span>
+                    <span className="p-5 text-2xl">{todo.title}</span>
                   </div>
                   <div>
-                    <span className="p-5">{todo.description}</span>
+                    <span className="p-5 text-xl">{todo.description}</span>
                   </div>
                 </div>
               </div>
@@ -94,5 +87,19 @@ function SharePage({ user, Logout }) {
     </div>
   );
 }
+
+SharePage.propTypes = {
+  user: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    email: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    armyType: PropTypes.string.isRequired,
+    armyRank: PropTypes.string.isRequired,
+    enlistment: PropTypes.string.isRequired,
+    discharge: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+  }).isRequired,
+  Logout: PropTypes.func.isRequired,
+};
 
 export default SharePage;
